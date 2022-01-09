056f035222b2a554cc2fda0e14e782e6a8e7ba6e
sheldonhull <sheldonhull@users.noreply.github.com>
2022-01-09 02:54:55 +0000

    build(devcontainer): use asdf instead of nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,35 @@
+# https://nix.dev/tutorials/declarative-and-reproducible-developer-environments
 with import <nixpkgs> { };
+
+# stdenv.mkDerivation {
+#   name = "devshell";
+#
+#   buildInputs = [
+#     pkgs.figlet
+#     pkgs.lolcat
+#   ];
+#
+#   # The '' quotes are 2 single quote characters
+#   # They are used for multi-line strings
+#   shellHook = ''
+#     figlet "Welcome!" | lolcat --freq 0.5
+#   '';
+# }
+# programs.zsh.enable;
+# users.extraUsers.codespace = {
+#     shell = pkgs.zsh;
+# };
+
 mkShell {
 
+  # Package names can be found via https://search.nixos.org/packages
   nativeBuildInputs = [
     direnv
+    bat
+    eva
+    fzf
+    git
+    zsh
   ];
 
   NIX_ENFORCE_PURITY = true;
